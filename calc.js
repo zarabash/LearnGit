5fd38fc4c6d673452a2c90eed64c27415b7f5da8
zara <zarashahhh@gmail.com>
2022-04-11 11:27:26 +0800

    calc.js is added

---
+++ calc.js
@@ -0,0 +1,20 @@
+let addition = function(x, y) {
+    return x + y;
+}
+ 
+let subtraction = function(x, y) {
+    return x - y;
+}
+ 
+let multiplication = function(x, y) {
+    return x * y;
+}
+ 
+let division = function(x, y) {
+    return x / y;
+}
+ 
+console.log(addition(20, 10))
+console.log(subtraction(20, 10))
+console.log(multiplication(20, 10))
+console.log(division(20, 10))
